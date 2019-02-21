f3711c94bcd78b73646ee1eaa611edd8a29bae83
hjqxcode <hjqxcode@gmail.com>
2019-02-21 21:57:07 +0800

    2019022121_first basic frame
    Signed-off-by: hjqxcode <hjqxcode@gmail.com>

---
+++ app/src/main/java/com/hjqxcode/airhockey/app/AirHockeyActivity.java
@@ -0,0 +1,45 @@
+package com.hjqxcode.airhockey.app;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hjqxcode.airhockey.renderer.AirHockeyRenderer;
+import com.hjqxcode.airhockey.util.Util;
+
+public class AirHockeyActivity extends AppCompatActivity {
+    private GLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Util.supportES20(this)) {
+            mGLSurfaceView = new GLSurfaceView(this);
+            mGLSurfaceView.setEGLContextClientVersion(2);
+            mGLSurfaceView.setRenderer(new AirHockeyRenderer());
+
+            setContentView(mGLSurfaceView);
+        } else {
+            Toast.makeText(this, "Exit app due to not support OpenGL ES 2.0", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onPause();
+        }
+    }
+}
---
+++ app/src/main/java/com/hjqxcode/airhockey/util/Util.java
@@ -0,0 +1,19 @@
+package com.hjqxcode.airhockey.util;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.util.Log;
+
+public class Util {
+    private static final String TAG = "Util";
+
+    public static boolean supportES20(Context context) {
+        final ActivityManager amg = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo cfgInfo = amg.getDeviceConfigurationInfo();
+        Log.v(TAG, "ConfigurationInfo.reqGlEsVersion: " + cfgInfo.reqGlEsVersion);
+
+        boolean supportES20 = cfgInfo.reqGlEsVersion >= 0x20000;
+        return supportES20;
+    }
+}
